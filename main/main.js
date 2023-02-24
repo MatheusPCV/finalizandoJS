e11da52563a22f0daca90b37a5bbcbbf38fa6da4
MatheusPCV <matheus.p.vieiraa@gmail.com>
2023-02-23 22:42:28 -0300

    versão 1.0

---
+++ main/main.js
@@ -0,0 +1,27 @@
+var btn = document.getElementById('run').addEventListener('click', submit)
+
+
+function submit() {
+    //ocultar página 1 e exibir página 2
+    let p1 = document.getElementById('page1')
+    let p2 = document.getElementById('page2')
+
+    p1.style.display = "none"
+    p2.style.display = "flex"
+    //inserir código em HTML
+    let codeHtml = document.getElementById('codeHtml').value
+    p2.innerHTML = codeHtml
+    //inserir o código em JS
+    let codeJs = document.getElementById('codeJs').value
+    document.getElementById('jsUser'), innerHTML = codeJs
+
+    /*
+    let codeJs = document.getElementById('codeJs').value
+    var tagScript = document.createElement("script").setAttribute("type", "text/javascript")
+
+    var textScript = document.createTextNode(codeJs)
+
+    tagScript.appendChild(textScript)
+    document.body.appendChild(tagScript) 
+    */
+}
