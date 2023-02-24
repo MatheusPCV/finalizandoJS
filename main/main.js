d0838621eb7bab342cefe72e1ac8e75330e6aeac
ImNotaStrawberry <samucasantana2003@gmail.com>
2023-02-24 19:17:31 -0300

    finalizado

--- main/main.js
+++ main/main.js
@@ -5,10 +5,14 @@ function submit() {
     //ocultar página 1 e exibir página 2
     let p1 = document.getElementById('page1')
     let p2 = document.getElementById('page2')
+    let header = document.getElementById('header')
+    let footer = document.getElementById('footer')
     let text1 = document.getElementById('codeHtml')
     let text2 = document.getElementById('codeJs')
     
     p1.style.display = "none"
+    header.style.display = "none"
+    footer.style.display = "none"
     p2.style.display = "flex"
     //inserir código em HTML
     let codeHtml = document.getElementById('codeHtml').value
@@ -16,6 +20,8 @@ function submit() {
     //inserir o código em JS
     let codeJs = document.getElementById('codeJs').value
     document.getElementById('jsUser').innerHTML = codeJs
+
+    document.getElementById("link").setAttribute("href", "/style/style2.css")
 
     /*
     let codeJs = document.getElementById('codeJs').value
